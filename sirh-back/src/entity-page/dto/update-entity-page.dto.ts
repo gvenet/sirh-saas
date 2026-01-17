72b9e4c5b741a878c871acc6eab7a69406cc6ae8
gvenet <guillaume.venet@septeo.com>
2026-01-17 22:13:25 +0100

    suppression back nest

--- sirh-back/src/entity-page/dto/update-entity-page.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEntityPageDto, CreatePageFieldDto } from './create-entity-page.dto';
-
-export class UpdateEntityPageDto extends PartialType(CreateEntityPageDto) {}
-
-export class UpdatePageFieldDto extends PartialType(CreatePageFieldDto) {}
--- sirh-back/src/generator/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './database-schema.service';
-export * from './file-generator.service';
-export * from './file-writer.service';
-export * from './relation.service';
-export * from './app-module.service';
-export * from './entity-parser.service';
